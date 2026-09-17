3f8cd210e5d2f051c31c4094d1fdce3f0b8dbd44
benjumeaquinteroalejandro-eng <benjumeaquinteroalejandro@gmail.com>
2026-09-17 11:41:47 -0500

    Menu Play

---
+++ MyUnity/Assets/_Main/Scripst/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void CargarEscena(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
